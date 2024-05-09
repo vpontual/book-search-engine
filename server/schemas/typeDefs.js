1c5ecad1ac1755e70f604261a774a72e7fc9017e
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-09 16:31:29 -0400

    completed typeDefs based on RESTFUL routes

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+`;
+
+module.exports = typeDefs;
