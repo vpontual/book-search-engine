9393c3f98987b61212c44f80442b216829e4dd7d
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-09 17:01:07 -0400

    updated resolves.js to add auth.js functionality

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User } = require("./models");
 const { signToken } = require("./utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
