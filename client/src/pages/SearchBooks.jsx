bda173ece12fc175fd05c8e4d8e87146d2e2d3a9
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-09 21:05:30 -0400

    removed reference to old API file

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -2,8 +2,11 @@ import { useState, useEffect } from "react";
 import { Container, Col, Form, Button, Card, Row } from "react-bootstrap";
 
 import Auth from "../utils/auth";
-import { saveBook, searchGoogleBooks } from "../utils/API";
-import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
+
+import { useMutation } from "@apollo/client";
+import { SAVE_BOOK } from "../utils/mutations";
+import { GET_ME } from "../utils/queries";
+import { getSavedBookIds, saveBookIds } from "../utils/localStorage";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -13,6 +16,10 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook] = useMutation(SAVE_BOOK, {
+    refetchQueries: [GET_ME, "me"],
+  });
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -29,6 +36,10 @@ const SearchBooks = () => {
     }
 
     try {
+      const searchGoogleBooks = (query) => {
+        return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+      };
+
       const response = await searchGoogleBooks(searchInput);
 
       if (!response.ok) {
@@ -56,6 +67,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    console.log(bookToSave);
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -65,11 +77,17 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const {
+        data: { _id: userId },
+      } = Auth.getProfile();
 
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
+      await saveBook({
+        variables: { ...bookToSave, userId },
+      });
+
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
